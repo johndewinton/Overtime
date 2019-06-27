4629b55736e1cc128aa75188a70ff22631e19a92
john de winton <john.dewinton@yahoo.co.uk>
2019-06-27 17:09:26 +0100

    modified posts controller to by refactoring code for best practice

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -13,21 +13,19 @@ describe 'navigate' do
   end
 
   describe 'creation' do
-    it 'has a new form that can be reached' do
-      visit new_post_path
-      expect(page.status_code).to eq(200)
-    end
+   before do
+    visit new_post_path
+   end
+   it 'has a new form that can be reached' do
+    expect(page.status_code).to eq(200)
+   end
 
-    it 'can be created from new form page' do
-      visit new_post_path
-
+   it 'can be created from new form page' do
       fill_in 'post[date]', with: Date.today
       fill_in 'post[rationale]', with: "Some rationale"
-
       click_on "Save"
-
       expect(page).to have_content("Some rationale")
-    end
+   end
   end
 end
 
